f5f657d9dc326606e9dbb3a55417da25e38dbe6a
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-20 22:32:43 +0100

    print combination

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(1, 10):
+    print("{:02d}, ".format(i), end='')
+for i in range(10, 90):
+    if i % 10 < i/10:
+        pass
+    elif i % 10 == i / 10:
+        pass
+    if i < 89:
+        print("{:02d}, ".format(i), end='')
+print("89")
