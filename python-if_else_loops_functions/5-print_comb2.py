d75a76c8f223c83c78dd57f5649873cd391bcddd
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-20 22:02:53 +0100

    print number from 0 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i == 0:
-        print("00,", end='')
-    elif i == 99:
+    if i == 99:
         print(" 99")
     else:
-        print(" %2d," % (i), end='')
+        print(" %02d," % (i), end='')
