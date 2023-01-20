ec3a0e04ce0ec655c449e308c46b08b874ddb7a4
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-20 23:37:15 +0100

    pow two integer

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    p = 1
-    if b < 0:
-        return (0.0001)
-    else:
-        for i in range(0, b):
-            p *= a
-        return (p)
+    return (a ** b)
