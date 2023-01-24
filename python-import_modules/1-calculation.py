b80b13f84841e12f2a98c0640735b1f0e7d355b4
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-24 21:00:31 +0100

    import functions from file

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator
+    print("10 + 5 = ",calculator.add(10, 5))
+    print("10 - 5 = ",calculator.sub(10, 5))
+    print("10 * 5 = ",calculator.mul(10, 5))
+    print("10 / 5 = ",calculator.div(10, 5))
+    
