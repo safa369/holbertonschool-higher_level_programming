77b2cf4ef0544c85ffa3884fa8f3268b1b748ee9
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-24 21:14:11 +0100

    import functions from file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import calculator
-    print("10 + 5 = ",calculator.add(10, 5))
-    print("10 - 5 = ",calculator.sub(10, 5))
-    print("10 * 5 = ",calculator.mul(10, 5))
-    print("10 / 5 = ",calculator.div(10, 5))
-    
+    print('{0} + {1} ='.format(10, 5),calculator.add(10, 5))
+    print('{0} - {1} ='.format(10, 5),calculator.sub(10, 5 ))
+    print('{0} * {1} ='.format(10, 5),calculator.mul(10, 5 ))
+    print('{0} / {1} ='.format(10, 5),calculator.div(10, 5 ))
