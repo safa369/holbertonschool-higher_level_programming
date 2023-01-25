4482b4bf5ba38eef756c0641b3b095ac0d59540b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-25 19:43:51 +0100

    command line arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,11 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 2:
-        print(len(sys.argv) - 1,"argument:", end='\n')
+        print(len(sys.argv) - 1, "argument:", end='\n')
         print('1: {}'.format(sys.argv[1]))
     elif len(sys.argv) == 1:
         print("0 arguments.")
     else:
         print(len(sys.argv) - 1, "arguments:", end='\n')
-        for i in range (1, len(sys.argv)):
+        for i in range(1, len(sys.argv)):
             print('{}: {}'.format(i, sys.argv[i]), end='\n')
-            
