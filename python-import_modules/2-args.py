89306062e379d3158def33073cb1f9bb4529174c
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-25 19:42:39 +0100

    command line arg

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        print(len(sys.argv) - 1,"argument:", end='\n')
+        print('1: {}'.format(sys.argv[1]))
+    elif len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print(len(sys.argv) - 1, "arguments:", end='\n')
+        for i in range (1, len(sys.argv)):
+            print('{}: {}'.format(i, sys.argv[i]), end='\n')
+            
