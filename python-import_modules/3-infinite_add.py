e9e92787a034e12fbf270ebeb15c40561c23b75b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-25 20:06:29 +0100

    addition of arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,11 +3,10 @@ if __name__ == "__main__":
     import sys
     i = 1
     sum = 0
-    n= len(sys.argv)
+    n = len(sys.argv)
     if n == 1:
         print('0')
     else:
         for i in range(1, n):
             sum += int(sys.argv[i])
         print(sum)
-    
