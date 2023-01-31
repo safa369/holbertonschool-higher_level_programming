b9d3edbdab9ed999a368a00f31763ead4f6ba0cc
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-31 11:33:49 +0100

    print list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range (0,len(my_list)):
+    for i in range(0, len(my_list)):
         print('{:d}'.format(my_list[i]), end='\n')
+
+
 if __name__ == "__main__":
     print_list_integer()
