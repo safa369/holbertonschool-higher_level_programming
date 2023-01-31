4ad2dbb3f893f1f99698bab5ce87944f4a756ba2
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-31 13:03:42 +0100

    new string

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    a = len(my_string)
+    my_newstring = ''
+    for i in range(0, a):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            my_newstring += ''
+        else:
+            my_newstring += my_string[i]
+    return (my_newstring)
+
+
+if __name__ == "__main":
+    no_c()
