328c9987a4b4cd99cf3bf14d1c3f1d06d90d315a
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-31 11:50:32 +0100

    replace an element in list

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    m = len(my_list)
+    if idx >= m or idx < 0:
+        return(my_list)
+    else:
+        my_list[idx] = element
+        return(my_list)
+
+
+if __name__ == "__main__":
+    replace_in_list()
