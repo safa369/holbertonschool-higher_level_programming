6fb96c985da553c0c78b275b29b3325f1f206492
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-31 11:42:23 +0100

    print element of list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     l = len(my_list)
-    if idx > l or idx < 0:
+    if idx >= l or idx < 0:
         return("None")
     else:
         return(my_list[idx])
