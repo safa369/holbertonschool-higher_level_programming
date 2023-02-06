ed53bd38a26ce4c9123c3845d08ed2a99e8fc547
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-02-06 18:53:32 +0100

    new tuple

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        tupple = (0, "None")
     else:
         s = 0
         for i in sentence:
