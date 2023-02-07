362a5b8f9b355d1caa203ba9df3ed19117eee9e5
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-02-07 10:56:08 +0100

    divisible by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     mys_list = []
     count = 0
     for i in range(0, len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             mys_list.insert(i, True)
         else:
             mys_list.insert(i, False)
