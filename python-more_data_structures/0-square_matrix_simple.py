f09d0123d1175ccb9ddf12022c88d1027584e72b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-02-08 11:58:04 +0100

    square matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,7 @@
 def square_matrix_simple(matrix=[]):
     if len(matrix) == 0:
         return matrix
-    else:
-        a = len(matrix)
-        
-        new_mat = [[matrix[j][i] ** 2 for i in range(a)] for j in range(len(matrix[0]))]
+    else:        
+        new_mat = []
+        new_mat = [[i * i for i in j]for j in matrix]
         return new_mat
