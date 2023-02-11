3f262eae86393f2fc26866cf0ce297b0e6821c4b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-02-12 00:15:49 +0100

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nb_arg = 0
-    for i in range (x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             nb_arg += 1
         except IndexError:
             break
+    print("")
     return nb_arg
