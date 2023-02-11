8bed607f37e4cb97202361707354f5e741c2249b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-02-12 00:13:53 +0100

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):    
-    nb_p = 0
-    for i in range(x):
+def safe_print_list(my_list=[], x=0):
+    nb_arg = 0
+    for i in range (x):
         try:
-            print("{}".format(my_list[i]), end='')
-            nb_p += 1
+            print("{}".format(my_list[i]), end="")
+            nb_arg += 1
         except IndexError:
             break
-    print("")
-    return nb_p
+    return nb_arg
