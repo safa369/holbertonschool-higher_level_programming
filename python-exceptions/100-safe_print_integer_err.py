ddff44a68d6f436c93217190084c0e627dff5080
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-02-13 22:39:11 +0100

    safe print integer error

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        print("Exception: {}".format(), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[0]), file=sys.stderr)
         return False
