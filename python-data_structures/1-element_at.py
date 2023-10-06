6bd2aaef8f504301da9fe43d6f2af6ce1913bf95
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-06 21:34:05 +0200

    commit

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 def element_at(my_list, idx):
     m = len(my_list) - 1
     if idx > m or idx < 0:
-        return("None")
+        return "None"
     else:
-        return(my_list[idx])
+        return  my_list[idx]
 
 
 if __name__ == "__main__":
