1aa8fc4e84fc423fd55888a19f14d36647af3892
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-06 21:32:14 +0200

    replace in list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,10 +2,10 @@
 def replace_in_list(my_list, idx, element):
     m = len(my_list) - 1
     if idx > m or idx < 0:
-        return(my_list)
+        return my_list
     else:
         my_list[idx] = element
-        return(my_list)
+        return my_list
 
 
 if __name__ == "__main__":
