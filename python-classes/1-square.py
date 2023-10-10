2549c4984b0ae08869fa670976c31fd8dfc1debb
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-10 16:51:26 +0200

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+""" This mmodule creates a Class named Square"""
 
 
 class Square:
-    """class defining square"""
+    """A class named Square
+    Attributes:
+    attribut1(size): size of square
+    """
     def __init__(self, size):
+        """
+        Args:
+        size: size for _size attribute of class instance
+        """
         self._size = size
