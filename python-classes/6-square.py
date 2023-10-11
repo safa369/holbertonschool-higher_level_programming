579d4b7bed7bff074eb61bc10d8dce7c8718b6b7
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-11 17:58:34 +0200

    square

---
+++ python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""a class model named square"""
+
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not all(isinstance(x, int) and x > 0 for x in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def are(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            for i in range(self.__size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
