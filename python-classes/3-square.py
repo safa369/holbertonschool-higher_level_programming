c8ad4cdc642212c9402dc76b68f85e16750fafa2
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-11 10:14:13 +0200

    square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,15 +3,13 @@ class Square:
     """Square:
      a class named square.
     Attributes:
-    attr1(size): size of square."""
-
-    def __init__(self, size=0):
-        """
-        Args:
+    attr1(size): size of square.Args:
         size(int): size for __size attribute of class
         Raises:
             TypeError: if size is not an integer.
             ValueError: if size is less than 0."""
+
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
