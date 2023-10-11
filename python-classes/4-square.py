4fd9c6424b5b86fef4e7b8453829ca2e5b637b83
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-11 13:49:11 +0200

    square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
