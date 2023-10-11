31b5d32d315dbe798ebce4951ea8d4f4ea401108
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-11 13:42:08 +0200

    print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""a class model named Square"""
+
+
+class Square:
+    """a class named square
+    Attributtes:
+        attr(size): size of square"""
+    def __init__(self, size=0):
+        """Initalise size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
