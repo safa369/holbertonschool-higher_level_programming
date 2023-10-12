317d56ac52b949541f75b513b6cd0f95b7586240
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-12 08:03:28 +0200

    square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,9 @@
 #!usr/bin/python3
-"""class model named Square"""
 
 
 class Square:
     """a class named square."""
     def __init__(self, size=0):
-        """Initialise size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -13,5 +11,4 @@ class Square:
         self.__size = size
 
     def area(self):
-        """return current square value"""
         return self.__size ** 2
