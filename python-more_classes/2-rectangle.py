4234fa08f124de4362a42a9bfb98318d4ebce513
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-12 21:42:20 +0200

    area and perimeter of triangle

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""a model class named Rectangle"""
+
+
+class Rectangle:
+    """a class named Rectangle
+    Attribute:
+        attr1(width): a width of rectangle of type integer
+        attr2(height): a height of rectangle of type integer
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        ar = self.__height * self.__width
+        return ar
+
+    def perimeter(self):
+        per = (self.__width + self.__height) * 2
+        return per
