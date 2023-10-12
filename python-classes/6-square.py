e1435d906455028e185241f965de7f4674493b00
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-12 13:34:01 +0200

    square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -38,6 +38,7 @@ class Square:
             for _ in range(self.__size):
                 print(" " * self.position[0], end="")
                 print("#" * self.size)
+            print()
 
     @property
     def position(self):
