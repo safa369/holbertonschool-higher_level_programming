0117cdafb5372ddabb4f03e0f74b36cb19444260
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-12 21:36:07 +0200

    definition of a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,7 +22,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -34,4 +35,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("heght must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
