d64eb6ea9d144290e34c495bf588bf79640b33bf
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-18 12:57:32 +0200

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""Module contain function to print name"""
 def say_my_name(first_name, last_name=""):
+    """function to print name
+    Args:
+        first_name(str): first name to print
+        last_name: last name
+    Returns: None
+    Raises:
+        if first_name or last_name are not strings"""
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
-    fl_name =f"My name is {first_name} {last_name}"
-    return fl_name
+    print("My name is {:s} {:s}".format(first_name, last_name))
