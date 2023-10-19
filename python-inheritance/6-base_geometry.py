5ec85551563f0f2b349904f38a14af8150bccb6f
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-19 15:35:23 +0200

    raise

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a model of base geometry"""
+
+
+class BaseGeometry:
+    def __init__(self):
+        pass
+
+    def area(self):
+        """function of area"""
+        raise Exception("area() is not implemented")
