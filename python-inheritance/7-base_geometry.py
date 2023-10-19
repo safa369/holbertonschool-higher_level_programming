ded50e57eedc1648177ed71dc0bbc40bb3d89286
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-19 21:22:02 +0200

    base geo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
         if type(name) is not str:
             name = str(name)
         if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
+            raise TypeError(f'{name} must be an integer')
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+            raise ValueError(f'{name} must be greater than 0')
