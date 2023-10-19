7ffd2e5a6f75f5726309fd61e3d110a2f1f02ea9
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-19 14:58:31 +0200

    issubclass

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" model of function to check if an object is an instance of class"""
+
+
+def inherits_from(obj, a_class):
+    """function to check an object
+    Attributes:
+        obj: the object
+        a_class: the class
+    Return: True or false"""
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
