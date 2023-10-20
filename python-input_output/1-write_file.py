2b85fbc59ca773a62eb65d8d0448e6ec0ba76b64
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-20 15:09:17 +0200

    write into a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module to write in file name"""
+
+
+def write_file(filename="", text=""):
+    """function write in file
+    Args:
+        filename(str): the name of file
+        text(str): the text to write it into the file"""
+    with open(filename, 'r+', encoding="utf8") as f:
+        return f.write(text)
