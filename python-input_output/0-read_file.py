80cd7226a8cb0d841fa2874f432675aaa2b18035
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-20 14:55:39 +0200

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""module of read a file"""
+
+
 def read_file(filename=""):
-    """function that read file"""
+    """function that read file
+    Args:
+        filename(str): the name of file to read it"""
     with open(filename, 'r', encoding="utf-8") as f:
         fo = f.read()
         print(fo)
