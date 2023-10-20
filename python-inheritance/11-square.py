97204c0a8943e628ab5de456cb9b91a16df2782e
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-20 12:53:45 +0200

    square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""model imported from 9-rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class named Square inherits from Rectangle"""
+    def __init__(self, size):
+        """function initialize new arguments
+        Args:
+            size(int): the size of square
+            """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """function calcul the area of square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """function return a string"""
+        return "[{}] {:d}/{:d}".format(Rectangle.__name__,
+                                       self.__size, self.__size)
