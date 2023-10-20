a96597cd498de0bb5d1118675c4abd1d1a35d09f
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-20 19:01:23 +0200

    load from json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""model to create an object from JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creatz an object from JSON file using JSON representation
+    Args:
+        filname: file """
+    with open(filename, 'r') as file:
+        json.load(file)
