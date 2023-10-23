60b6f8183c6624e6e639364799167e19472d5764
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-23 13:16:05 +0200

    add, save, load

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""module to add, save, and load"""
 import json
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-filname = "add_item.json"
-with open(filname, 'a+', encoding="utf8") as f:
-    arguments = sys.argv[1:]
-    file = load_from_json_file(filname)
-    file.append(arguments)
-    save_to_json_file(file, filname)
+if __name__ == "__main__":
+    try:
+        argm = sys.argv[1:]
+        filename = "add_item.json"
+        file = load_from_json_file(filename)
+        file.extend(argm)
+        save_to_json_file(file, filename)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        file = argm
+        save_to_json_file(file, filename)
