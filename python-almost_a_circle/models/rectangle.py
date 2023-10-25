5743418e4116ef6e1884be8620f24e4af529cbc9
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-25 23:05:46 +0200

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,10 @@ class Rectangle(Base):
             y: integer
             id: identifier"""
         super().__init__(id=id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
