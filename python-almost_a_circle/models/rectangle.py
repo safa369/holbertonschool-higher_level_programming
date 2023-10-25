ce3ce2b88c35644f6221bdd746aec127e6843b86
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-25 17:42:43 +0200

    rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""module of rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class named rectangel inherts from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """private variable"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """getter method to get properties"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter method to change the value"""
+        self.__width = value
+
+    @property
+    def height(self):
+       """getter method to get properties"""
+       return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter method to change the value"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter method to get properties"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ setter method to change the value"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter method to get properties"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ setter method to change the value"""
+        self.__y = value
