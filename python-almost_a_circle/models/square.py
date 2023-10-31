bc2c36136e5e141a5493bd57f620b9172cc3534f
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-10-31 18:48:22 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,9 +5,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """class of swquare onherites from Rectanglze"""
     def __init__(self, size, x=0, y=0, id=0):
+        """initialize function"""
         self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """str function"""
         return(f'[Square] ({self.id}) {self.x}/{self.y} - {self.height}')
