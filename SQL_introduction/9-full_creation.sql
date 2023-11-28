68acac19acf6ec1cad2c0206d1e34f97955fe224
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-11-28 14:23:01 +0100

    create table and insert into it

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create second table in databease.
-CREATE TABLE IF NOT EXISTS second_table('id' INT, 'name' VARCHAR(256), 'score' INT);
-INSERT INTO second_table('id', 'name', 'score') VALUES(1, 'John', 10);
-INSERT INTO second_table('id', 'name', 'score') VALUES (2, 'Alex', 3);
-INSERT INTO second_table('id', 'name', 'score') VALUES(2, 'Bob', 14);
-INSERT INTO second_table('id', 'name', 'score') VALUES (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table(id, name, score) VALUES(2, 'Bob', 14);
+INSERT INTO second_table(id, name, score) VALUES (4, 'George', 8);
