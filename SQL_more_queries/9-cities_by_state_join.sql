2cccd0fd6537a05bb8d39d4cef0785ab3dfe776c
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-11-29 17:36:47 +0100

    join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- cities by state join.
 SELECT cities.id, cities.name, states.name 
 FROM cities 
-JOIN states ON cities.id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
