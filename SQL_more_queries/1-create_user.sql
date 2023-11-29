96962f5d4471116e44ea64ff1fd5ae425af6a3bc
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-11-29 11:44:10 +0100

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- create user.
 CREATE USER 
 IF NOT EXISTS 'user_0d_1'@'localhost'  
-IDENTIFIED BY 'User_0d_1_pwd';
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANTS
 ALL PRIVILEGES ON *.*
 TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'User_0d_1_pwd';
+IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
