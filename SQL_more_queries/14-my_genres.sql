7e7333186e08f508cfe6b6e7f9fc027c55330ab6
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-11-30 14:43:57 +0100

    lists all genre

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ FROM tv_genres AS tg
 JOIN tv_show_genres AS tsg ON tg.id=tsg.genre_id
 JOIN tv_shows AS ts ON ts.id = tsg.show_id
 WHERE ts.title = 'Dexter'
-ORDER BY tg.name DESC;
+ORDER BY tg.name;
