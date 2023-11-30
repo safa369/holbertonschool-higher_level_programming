a6ce926cd3522177d41d31072bc604168fa69e48
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-11-30 12:31:09 +0100

    show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- show genre.
-SELECT title, genre_id FROM tv_shows
-join tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT title, genre_id FROM tv_shows AS ts
+join tv_show_genres AS tg ON ts.id=tg.show_id
+ORDER BY ts.title, tg.genre_id;
