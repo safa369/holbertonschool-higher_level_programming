befe4df37e9ee77f773fece5af6e9cfa59ed488e
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-05 09:54:18 +0100

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import sys
 
 
 if __name__ == "__main__":
-    db=MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    db=MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwod=sys.argv[2], db=sys.argv[3])
     mycursor = db.cursor()
     x = mycursor.execute("SELECT * FROM states ORDER BY states.id")
     for i in x:
