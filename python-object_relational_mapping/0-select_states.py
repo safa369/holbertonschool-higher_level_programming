c77c28497182c54ddf9ac1af7ea897c91f6644df
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-05 11:24:30 +0100

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,6 @@
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
     db=MySQLdb.connect(host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3])
     mycursor = db.cursor()
