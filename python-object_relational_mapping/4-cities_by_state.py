4c9fd5576341a95f7a1e5161050a44ad27689947
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-06 15:42:36 +0100

    list cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all cities from databse"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=argv[1],
+                        password=argv[2],
+                        db=argv[3])
+    myc = db.cursor()
+    myc.execute("SELECT cities.id, states.name "
+                "FROM cities join states ON states.id=cities.id "
+                "ORDER BY cities.id")
+    fet = myc.fetchall()
+    for x in fet:
+        print(x)
+    db.close()
