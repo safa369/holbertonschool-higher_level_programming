eb9aebef0edb212921ab9f39e592590b2adf2e3a
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-06 11:02:26 +0100

    lists name of states bgining witn N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], password=argv[2], db=argv[3])
     mycursor = db.cursor()
-    x = mycursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-    for i in range(x):
-        print(mycursor.fetchone())
+    mycursor.execute("SELECT * FROM states WHERE\
+                      name LIKE 'N%' ORDER BY states.id")
+    x = mycursor.fetchall()
+    for i in x:
+        print(i)
+db.close()
