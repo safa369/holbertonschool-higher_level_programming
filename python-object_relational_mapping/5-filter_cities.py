cfcba2484a676ebe806520ea5f544300233e33c6
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-06 17:02:07 +0100

    filter cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,30 +10,24 @@ from sys import argv
 
 names = argv[4]
 if __name__ == "__main__":
-    """ connect """
     db = MySQLdb.connect(
                         host="localhost",
                         port=3306,
                         user=argv[1],
                         password=argv[2],
                         db=argv[3])
-    """ cursor """
     myc = db.cursor()
-    """ execute """
     myc.execute("SELECT cities.name "
                 "FROM cities join states "
                 "ON states.id = cities.state_id "
                 "AND states.name = %s "
                 "ORDER BY cities.id", (names, ))
-    """ fetchall """
     fet = myc.fetchall()
     idx = 0
-    """print"""
     for x in fet:
         if idx != 0:
             print(',', end=' ')
         print(x[0], end='')
         idx += 1
     print('')
-    """close"""
     db.close()
