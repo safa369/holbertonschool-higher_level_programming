ab44c21c25d8434556d27a155138c963eece9277
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-07 18:19:59 +0100

    model state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class of a table named states
+    attribute:
+        id: integer.
+        name: max 128 charac """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
