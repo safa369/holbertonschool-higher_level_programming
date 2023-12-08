d7056c2de5e9737716ad1f0a7a43199e239fab3b
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-08 21:34:09 +0100

    state filter

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""list all states"""
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%'))\
+        .order_by(State.id.asc())
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
