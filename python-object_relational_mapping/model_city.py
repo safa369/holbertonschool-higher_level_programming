711508fba3679523d421da0706eca559842f979c
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-12-09 20:16:29 +0100

    model city

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""file contain th class definition of a City"""
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+Base = declarative_base()
+
+
+class City(Base):
+    """class that define a table named city
+    Attribute:
+        id: integer
+        name: string
+        states_id: foreign key of states.id"""
+    __tablename__="cities"
+    id = (Column(Integer, nullable=False, primary_key=True))
+    name = (Column(String(128), nullable=False))
+    state_id = (Column(Integer, ForeignKey('states.id')))
