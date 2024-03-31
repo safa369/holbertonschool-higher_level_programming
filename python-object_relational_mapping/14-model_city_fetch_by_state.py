f0acac63d67adb379fa82f1bce0a9aec6f88a221
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-03-31 15:30:00 +0200

    fetch

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,21 +7,22 @@ from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 def main():
-    engine=create_engine('mysql+mysqldb://{}: {}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]),  pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cit = session.query(City).join(State).order_by(City.id.asc()).all()
-
-    for c in cit:
-        print('{}: ({}) {}'
-              .format(c.State.name, c.City.id, c.City.name))
+    results = session.query(City, State).\
+        filter(City.state_id == State.id).all()
+    for city, state in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.commit()
     session.close()
 
- 
+
 if __name__ == '__main__':
     main()
