733d9716ea8316a0dcaa383742f8dafc823dc1ab
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-03-31 03:06:37 +0200

    model

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -14,6 +14,7 @@ class City(Base):
         name: string
         states_id: foreign key of states.id"""
     __tablename__ = "cities"
-    id = (Column(Integer, nullable=False, primary_key=True))
-    name = (Column(String(128), nullable=False))
-    state_id = (Column(Integer, ForeignKey('states.id')))
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
